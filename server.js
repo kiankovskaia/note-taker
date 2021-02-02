03682da13600a4249ff7ab79620b8eef3d2c54fe
yourdilemma <k.iankovskaia@gmail.com>
2021-02-01 20:27:21 -0500

    completed create notes and delete routes

--- server.js
+++ server.js
@@ -1,79 +1,112 @@
 // Bring in Node Dependencies
-const express = require('express');
-const path = require('path')
-const fs = require('fs')
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
 
-// JSON data
-let db = require('./db/db.json')
+// Utilizing node.js util function for promisification in order to convert callbacks to promises, using readFile and writeFile within GETs, POSTs, and DELETEs.
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
-// Setup Express
+// Joining db.json file using noteArray variable
+const noteArray = path.join(__dirname, "db/db.json");
+console.log(noteArray);
+
+// Setup Express App
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Setup Middleware to parse data
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static('public'))
-
-
-// notes route to notes.html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+// connect style and script sheets that reside in the public folder.
+app.use(express.static("public"));
 
 // notes api route to return json data : GET method
-app.get('/api/notes', (req, res) => {
-    res.json(db)
-})
 
+app.get("/api/notes", function (req, res) {
+  // Convert data using parse
+  readFile(noteArray, "utf8")
+    .then((data) => res.json(JSON.parse(data)))
+    .catch((error) => {
+      // If error arises, display
+      console.log(`Error reading file! ${noteArray}`);
+    });
+});
 
-// notes api route to add json data : POST method
-app.post('/api/notes', (req, res) => {
-    
-  const noteId = db.length + 1;
-
-    
-    const newNote = {
-        id: noteId,
-        title: req.body.title,
-        text: req.body.text,
+// Generating IDs to sort through array of objects. Setting original ID value at 0 will let us build up order of notes taken and target the IDs that we want to select, shown later in the delete function.
+function generateNewId(currentNotes) {
+  let highestId = 0;
+  currentNotes.forEach((note) => {
+    if (note.id > highestId) {
+      highestId = note.id;
     }
-    db.push(newNote) 
-    res.json(newNote)
+  });
+  return highestId + 1;
+}
 
+// POST - Create New Notes
+app.post("/api/notes", function (req, res) {
+  readFile(noteArray, "utf8")
+    .then((data) => {
+      let notes = JSON.parse(data);
+      // Spread operators are used to expand elements of an iterable (think array-like) into situations where multiple elements can fit.
+      const newNote = { ...req.body, id: generateNewId(notes) };
+      notes.push(newNote);
+      writeFile(noteArray, JSON.stringify(notes))
+        .then(() => {
+          res.json(newNote);
+        })
+        .catch((error) => {
+          console.log(`Error writing file! ${noteArray}`);
+        });
+    })
+    .catch((error) => {
+      console.log(`Error reading file! ${noteArray}`);
+    });
+});
 
-})
-
-
-// use fs to read the file
-
-
-
-// parse the file and add the note
-
-
-
-// rewrite the file, using fs.writefile
-
-
-
-
-// initial route to index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+// DELETE
+app.delete("/api/notes/:id", function (req, res) {
+  // generating variable to reference request parameter ID - parsed as integer
+  const idToBeDeleted = parseInt(req.params.id);
+  readFile(noteArray, "utf8")
+    .then((data) => {
+      let notes = JSON.parse(data);
+      // Filtering newNote to ensure its ID doesn't equate to  idToBeDeleted
+      // REMINDER: filter() method generates a new array with all elements installed by the function provided
+      notes = notes.filter((newNote) => newNote.id !== idToBeDeleted);
+      writeFile(noteArray, JSON.stringify(notes))
+        .then(() => {
+          res.send("Got a DELETE request at /user");
+        })
+        .catch((error) => {
+          console.log(`Error writing file! ${noteArray}`);
+        });
+    })
+    .catch((error) => {
+      console.log(`Error reading file! ${noteArray}`);
+    });
 });
 
 
+// notes route to notes.html
 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
+// initial route to index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
+// listener
 
-app.listen(port, (err)=> {
-    if (err){
-        console.log(err)
-    }
-    else {
-        console.log(`Server is listening on Port: ${port}`)
-    }
-})
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is listening on Port: ${port}`);
+  }
+});
